d1bce01c1d16e035b6b2180dac15a6fe2b21d2dd
Carolina <80101811+Caholive1ra@users.noreply.github.com>
2026-08-14 14:34:05 -0300

    chore(infra): gera maven wrapper 3.9.9 e cria classe main (#29)
    * chore: add initial pom.xml to enable maven wrapper generation
    
    * chore(infra): add maven wrapper 3.9.9 and spring boot main class
    
    * chore(infra): versiona .mvn/wrapper/maven-wrapper.properties e corrige .gitignore

---
+++ src/main/java/br/com/fiap/aguiabranca/FiapAguiaBrancaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.aguiabranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapAguiaBrancaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiapAguiaBrancaApplication.class, args);
+    }
+
+}
